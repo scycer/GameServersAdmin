b09efb9de0455ea373a7419a1941d87c765d7a75
dan <scycer1@hotmail.com>
2020-11-03 08:43:10 +1100

    add logging

--- gameserverVM/http-service/index.js
+++ gameserverVM/http-service/index.js
@@ -4,9 +4,23 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+// Log all reqs
+app.use((req, res, next) => {
+  console.log(`Request: ${req.url}`)
+  next()
+} )
+
+
 app.get('/healthcheck', (req, res) => {
   res.send({status: "Running"})
 })
+
+// Log all unhandled route reqs
+app.use((req, res, next) => {
+  console.error(`Not a route - Request: ${req.url}`)
+  next()
+} )
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
